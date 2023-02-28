5bb41e792fe1b1587a15ff867ee4c1d235b4883c
Khushbu253 <khushbupankhaniya25@gmail.com>
2023-02-28 16:36:50 +0530

    ..rest api

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,15 @@ const inter = Inter({ subsets: ['latin'] })
 
 function Home(props) {
   console.log(props.data,props.seo,"dataaaa")
-  const metaDetails=props.seo.seoDetails.find((detail)=>detail.pageName==='home')
+  const metaDetails=props.seo
+  // const metaDetails=props.seo.seoDetails.find((detail)=>detail.pageName==='home')
   // console.log(metaDetails,"meta")
   return (
     <>
       <Head>
-        <title>{metaDetails.metaTitle}</title>
-        <meta name="description" content={metaDetails.metaDescription}/>
-        <meta name="keywords" content={metaDetails.metaKeywords}/>
+        <title>{metaDetails.title}</title>
+        <meta name="description" content={metaDetails.meta_description}/>
+        <meta name="keywords" content={metaDetails.meta_keyword}/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -29,7 +30,7 @@ function Home(props) {
 export default Home;
 export async function getServerSideProps() { 
   const res = await fetch(`https://api.cybercomcreation.com/address`)
-  const seo =await fetch(`https://api.cybercomcreation.com/seo`)
+  const seo =await fetch(`http://192.168.0.204:1330/seo-rest`)
     // Pass data to the page via props
    return { props: {
                       data :await res.json(),
