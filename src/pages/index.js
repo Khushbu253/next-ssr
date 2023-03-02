c316b416c4777b694b41a20a814e5a2f141f026a
Khushbu253 <khushbupankhaniya25@gmail.com>
2023-03-02 17:05:34 +0530

    completed home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,16 @@ import client from 'ApolloClient/client'
 import Layout from '@/components/App/Layout'
 import Navbar from '@/components/App/Navbar'
 import Banner from '@/components/index/Banner'
+import OurSolutions from '@/components/index/OurSolutions'
+import OurServices from '@/components/index/OurServices'
+import OurFeatures from '@/components/index/OurFeatures'
+import RecentProjects from '@/components/index/RecentProjects'
+import Pricing from '@/components/index/Pricing'
+import Testimonials from '@/components/index/Testimonials'
+import Partner from '@/components/index/Partner'
+import Footer from '@/components/App/Footer'
+import ProjectStartArea from '@/components/index/ProjectStartArea'
+import OurBlog from '@/components/index/OurBlog'
 
 function Home(props) {
   console.log(props.footer,"footer data")
@@ -16,6 +26,25 @@ function Home(props) {
     <Layout pageName="home">
       <Navbar />
       <Banner data={props.banner}/>
+      <OurSolutions
+          data={props.solution}
+          serviceSolutions={props.serviceSolutions}
+        />
+      {/* <OurServices
+          servicesOne={props.servicesOne}
+          servicesTwo={props.servicesTwo}
+      /> */}
+       <OurFeatures data={props.features} />
+       {/* <RecentProjects
+          recentProjects={props.recentProjects}
+          projects={props.projects}
+        /> */}
+        <Pricing />
+        <Testimonials Testimonials={props.Testimonials} />
+        <Partner Partner={props.Partner} />
+        {/* <OurBlog OurBlog={props.OurBlog} /> */}
+        <ProjectStartArea ProjectStartArea={props.ProjectStartArea} />
+        <Footer footer={props.footer} />
     </Layout>
   </ApolloProvider>
     // <>
@@ -39,11 +68,42 @@ export async function getServerSideProps() {
   try{
     const footer = await fetch(`${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}address`);
     const banner = await fetch(`${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}default-banner`);
-
+    const solution = await fetch(`${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}solution`);
+    const serviceSolutions = await fetch(
+      `${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}service-solutions`
+    );
+    const servicesOne = await fetch(
+      `${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}services-one`
+    );
+    const servicesTwo = await fetch(
+      `${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}services-two`
+    );
+    const features = await fetch(`${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}our-features`);
+    const RecentProjects = await fetch(
+      `${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}recent-projects`
+    );
+    const Testimonials = await fetch(
+      `${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}testimonials`
+    );
+    const Partner = await fetch(`${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}partner`);
+    const OurBlog = await fetch(`${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}blogs`);
+    const ProjectStartArea = await fetch(
+      `${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}start-your-project`
+    );
     return{
       props:{
         footer: await footer.json(),
         banner: await banner.json(),
+        solution: await solution.json(),
+        serviceSolutions: await serviceSolutions.json(),
+        servicesOne: await servicesOne.json(),
+        servicesTwo: await servicesTwo.json(),
+        features: await features.json(),
+        recentProjects: await RecentProjects.json(),
+        Testimonials: await Testimonials.json(),
+        Partner: await Partner.json(),
+        OurBlog: await OurBlog.json(),
+        ProjectStartArea: await ProjectStartArea.json(),
       }
     }
   }catch (error) {
