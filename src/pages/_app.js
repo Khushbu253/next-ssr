45f0d491c2bf95b3f0069ee0a0e4a3def92a1461
Khushbu253 <khushbupankhaniya25@gmail.com>
2023-03-09 10:18:07 +0530

    added font link

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,7 +9,6 @@ import "../assets/css/animate.min.css"
 import "../assets/css/boxicons.min.css"
 import "../assets/css/flaticon.css"
 import '../../node_modules/react-modal-video/css/modal-video.min.css'
-// import '.../../node_modules/react-modal-video/css/modal-video.min.css'
 import 'react-accessible-accordion/dist/fancy-example.css'
 import 'react-image-lightbox/style.css'
 import "../assets/css/style.css"
